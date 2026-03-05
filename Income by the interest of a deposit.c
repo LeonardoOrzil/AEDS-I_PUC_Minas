e68f8432b099d2fd7615b5c6cfa38b5bcb324928
Leonardo Orzil <lorzil2008@gmail.com>
2026-03-05 07:46:24 -0300

    Update prompts for interest calculation

--- Income by the interest of a deposit.c
+++ Income by the interest of a deposit.c
@@ -3,13 +3,13 @@ int main () {
     float a, b, c, d, x;
     printf("Insert the value of your deposit\n");
     scanf("%f", &a);
-    printf("Insert now the percent of the fees\n");
+    printf("Insert now the percent of the interest\n");
     scanf("%f", &x);
 
     c= (a/100)*x;
     d= a+c;
 
-    printf("The value of your income will be %.2f. In total you will have %.2f\n", c, d);
+    printf(" Value the income: %.2f\n Total value after the income: %.2f\n", c, d);
 
 return 0;
 }
