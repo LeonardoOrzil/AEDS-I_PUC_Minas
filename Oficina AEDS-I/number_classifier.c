1042ab6fce36187b427214a7b86cd865f6c20500
Leonardo Orzil <lorzil2008@gmail.com>
2026-03-23 14:11:04 -0300

    Create number_classifier.c

---
+++ Oficina AEDS-I/number_classifier.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int conti, contn, contp, contz, valor;
+
+    printf("Type how many numbers will you type\n");
+    scanf("%d", &conti);
+    printf("type the numbers\n");
+    for(int i=0; i<conti; i++){
+        scanf("%d", &valor);
+        if(valor==0){
+            contz++;
+        }else if(valor>0){
+            contp++;
+        }else{
+            contn++;
+        }
+    }
+    printf("%d Positivos \n%d Negativos \n%d zeros", contp,contn, contz);
+
+return 0;
+}
