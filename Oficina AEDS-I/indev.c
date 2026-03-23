9f72a76b500c76b914e6f6862f4efd9eb8eacc2c
Leonardo Orzil <lorzil2008@gmail.com>
2026-03-23 15:05:00 -0300

    Create indev.c

---
+++ Oficina AEDS-I/indev.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    int num1, div39, div2, div5;
+
+    printf("Type 10 numbers\n");
+
+    for(int i=0; i<10; i++){
+        scanf("%d", &num1);
+        printf("%d is divisible by", num1);
+        if (num1%3==0 && num1%9==0){
+            printf(" 3 and 9\n");
+            div39++;
+        }
+        if (num1%2==0){
+            printf(" 2\n");
+            div2++;
+        }
+        if (num1%5==0){
+            printf(" 5\n");
+            div5++;
+        }
+        if (num1%3!=0 && num1%9!=0  num1%2!=0 && num1%5!=0){
+            printf(" none of the required numbers\n");
+        }
+
+    }
+    printf("%d is the amount of numbers divisible by e and 9\n%d is the amount of numbers divisible by 2\n%d is the amount of numbers divisible by 5", div39, div2, div5);
+
+
+return 0;
+}
