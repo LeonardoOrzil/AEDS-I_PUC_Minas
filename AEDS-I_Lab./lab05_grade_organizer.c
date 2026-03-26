2e3ad847202b1112b91794a9eb02d3d7ba17c620
Leonardo Orzil <lorzil2008@gmail.com>
2026-03-26 08:05:41 -0300

    Update lab05_grade_organizer.c

--- AEDS-I_Lab./lab05_grade_organizer.c
+++ AEDS-I_Lab./lab05_grade_organizer.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    int grades=0, students=0, med, max=0, min=10000, total=0;
+    float grades=0, students=0, med, max=0, min=10000, total=0;
     printf("Insert the grades of the students\n Type -1 when you finish\n");
 
     while(grades!=-1){
-        scanf("%d", &grades);
+        scanf("%f", &grades);
         if(grades!=-1){
             students++;
             total=grades+total;
@@ -18,6 +18,6 @@ int main(){
         }
     }
     med=total/students;
-    printf("%d is the total of students\n%d is the average of the grades \n%d is the greatest grade \n%d is the worst grade\n", students, med, max, min);
+    printf("%.0f is the total of students\n%.2f is the average of the grades \n%.2f is the greatest grade \n%.2f is the worst grade\n", students, med, max, min);
 return 0;
 }
